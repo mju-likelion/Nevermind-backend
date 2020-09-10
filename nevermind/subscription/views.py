ace74207789e5f50053cc329a44e823109d9cadb
13circle <13circle97@gmail.com>
2020-09-10 14:09:06 +0000

    Set API response functions for subscriptions
    - Only empty functions
    - Will fill out codes after handling subscription is specified

--- nevermind/subscription/views.py
+++ nevermind/subscription/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def add(req):
+  #
+  return JsonResponse({})
+
+
+def get(req):
+  #
+  return JsonResponse({})
+
+
+def update(req):
+  #
+  return JsonResponse({})
+
+
+def delete(req):
+  #
+  return JsonResponse({})
